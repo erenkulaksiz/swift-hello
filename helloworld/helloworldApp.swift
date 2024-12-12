f4c2ff850a03c1f732138a969d7582935764e019
Eren Kulaksız <erenkulaksz@gmail.com>
2024-12-12 12:01:37 +0300

    Initial Commit

---
+++ helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by erenkulaksiz on 12.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
